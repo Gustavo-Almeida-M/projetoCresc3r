8564838364efd1dfcc46f0b7d2f9ace7b49d548d
Gustavo <gustavoam916@gmail.com>
2025-08-25 22:27:18 -0300

    Adicionando um cabeçalho importado do site tailwindcss

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+
     </head>
     <body>
         <x-header />
